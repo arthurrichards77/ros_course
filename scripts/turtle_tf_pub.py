6ee4e09a86ad04b864a3dd2d8bdb1d3f24ddc434
Arthur <arthur.richards@bristol.ac.uk>
2023-12-06 11:09:02 +0000

    Updated the URDF to account for fact that ROS Noetic stopped supporting tf_prefix in robot_state_publisher.  Attempts at simple workaround for showing two turtles in RViz have failed.

--- scripts/turtle_tf_pub.py
+++ scripts/turtle_tf_pub.py
@@ -4,20 +4,27 @@ roslib.load_manifest('ros_course')
 import rospy
 import tf
 import turtlesim.msg
- 
-def handle_turtle_pose(msg, turtlename):
-    br = tf.TransformBroadcaster()
-    br.sendTransform((msg.x, msg.y, 0),
-                     tf.transformations.quaternion_from_euler(0, 0, msg.theta),
-                     rospy.Time.now(),
-                     turtlename+'/base_link',
-                     "world")
- 
+
+class TurtleTfPublisher:
+
+    def __init__(self):
+        rospy.init_node('turtle_tf_broadcaster', anonymous=True)
+        self.turtle_name = rospy.resolve_name('turtle1')
+        self.tf_broadcaster = tf.TransformBroadcaster()
+    
+    def pose_callback(self, msg):
+        self.tf_broadcaster.sendTransform((msg.x, msg.y, 0),
+                                          tf.transformations.quaternion_from_euler(0, 0, msg.theta),
+                                          rospy.Time.now(),
+                                          self.turtle_name+'/base_link',
+                                          "world")
+
+    def run(self):
+        rospy.Subscriber('turtle1/pose',
+                         turtlesim.msg.Pose,
+                         self.pose_callback)
+        rospy.spin()
+
 if __name__ == '__main__':
-    rospy.init_node('turtle_tf_broadcaster', anonymous=True)
-    turtlename=rospy.resolve_name('turtle1')
-    rospy.Subscriber('turtle1/pose',
-                     turtlesim.msg.Pose,
-                     handle_turtle_pose,
-                     turtlename)
-    rospy.spin()
+    tp = TurtleTfPublisher()
+    tp.run()
